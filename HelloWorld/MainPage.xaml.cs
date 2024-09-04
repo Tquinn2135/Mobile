2ec43bb8a1d220c91b9cd0a5c649a56396a28773
Thomas Quinn <tquinn2135@gmail.com>
2024-09-04 15:02:06 -0700

    TPQ First App Hello World

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Hello_OnClicked(object sender, EventArgs e)
+    {
+        if (lblDisplay.Text == "Hello World")
+        {
+            lblDisplay.Text = "This is cool!";
+        }
+        else
+        {
+            lblDisplay.Text = "Hello World";
+        }
+        
+    }
+}
